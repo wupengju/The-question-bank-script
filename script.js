018bb8cb24dae550875f0f1f7fbacb42fb322968
wupengju <3145679267@qq.com>
2017-05-12 09:22:38 +0800

    What: 对构造函数的默认参数处理进行重新编码和优化，支持多种调用方式: 1. 自主设置配置参数(可设置部分) 2. 不传参(采用默认参数)
    Why:
    1. 更多样友好的调用方式
    2. 让代码的职责更加具体细致
    
    How:
    1. 封装函数
    2. 传递参数在满足条件下进行覆盖默认参数

--- script.js
+++ script.js
@@ -35,7 +35,7 @@
 			this.$chapterOptions = this.$chapter.getElementsByTagName('option');
 			this.arrChapters = this.setArrChapters(); // 章节项目的信息组成的对象数组
 			this.curChapterIndex = this.setCurChapterIndex(); // 正在刷的题目的章节数
-			this.arrErrorChapters = options.arrErrorChapters; // 有错误章节项目的序列数组
+			this.arrErrorChapters = options.arrErrorChapters || []; // 有错误章节项目的序列数组
 
 			return this;
 		}
@@ -43,28 +43,76 @@
 
 	Answer.fn.init.prototype = Answer.fn;
 
+	// 添加默认参数
+	Answer.addDefaultOptions = function (options, arrOptionsKeys) {
+
+		var defaultOptions = {
+			arrFrames: ['topmain', 'main'], 
+			validateStr: '此章节下的程序已读取结束',
+			chapterSelector: 'cChapter',
+			promptInfoSelector: 'divInfo',
+			arrErrorChapters: [3]
+		};
+
+		if (arguments.length === 1) {
+			return defaultOptions;
+		} else {
+			for (var i = 0, len = arrOptionsKeys.length; i < len; i++) {
+				defaultOptions[ arrOptionsKeys[ i ] ] = options[ arrOptionsKeys[ i ] ]; // 直接覆盖
+			}
+			return defaultOptions;
+		}
+	};
+
 	// 检测构造函数的配置参数
 	Answer.detectOptions = function (options) {
 		var ret = [],
-				checkSign = false;
+				checkSign = false,
+				arrOptionsKeys = [];
 
-		if (!Answer.detectDataTypes(options, 'Object')) {
-			console.log('请传入对象参数.');
-			return false;
-		}
+		if (options === undefined) { // 添加默认参数
+			return Answer.addDefaultOptions(options);
+		} else { // 检测配置参数
+			if (!Answer.detectDataTypes(options, 'Object')) {
+				console.log('请传入对象参数.');
+				return false;
+			}
 
-		ret.push(Answer.detectDataTypes('arrFrames' in options && options.arrFrames, 'Array'));
-		ret.push(Answer.detectDataTypes('validateStr' in options && options.validateStr, 'String'));
+			// 按照默认参数的设置顺序检测参数
+			if ('arrFrames' in options) { // 先判断参数的存在性，再判断参数的类型
+				ret.push(Answer.detectDataTypes(options.arrFrames, 'Array'));
+			}
+			
+			if ('validateStr' in options ) { // 先判断参数的存在性，再判断参数的类型
+				ret.push(Answer.detectDataTypes(options.validateStr, 'String'));
+			}
 
-		checkSign = ret.every(function (item) {
-			return item === true ? true : false;
-		});
+			if ('chapterSelector' in options) { // 先判断参数的存在性，再判断参数的类型
+				ret.push(Answer.detectDataTypes(options.chapterSelector, 'String'));
+			}
+			
+			if ('promptInfoSelector' in options ) { // 先判断参数的存在性，再判断参数的类型
+				ret.push(Answer.detectDataTypes(options.promptInfoSelector, 'String'));
+			}
 
-		if (checkSign) {
-			return options;
-		} else {
-			console.log('传入的参数不符合规范.');
-			return false;
+			if ('arrErrorChapters' in options) { // 先判断参数的存在性，再判断参数的类型
+				ret.push(Answer.detectDataTypes(options.arrErrorChapters, 'Array'));
+			}
+
+			checkSign = ret.every(function (item) {
+				return item === true ? true : false;
+			});
+
+			if (checkSign) {
+				arrOptionsKeys =  Object.keys(options); // 可能有兼容性问题
+				if (arrOptionsKeys.length < 5) { // 优化：只有当传入配置参数的属性不够的时候才会添加默认参数
+					options = Answer.addDefaultOptions(options, arrOptionsKeys);
+				}
+				return options;
+			} else {
+				console.log('传入的参数不符合规范.');
+				return false;
+			}
 		}
 	};
 
@@ -134,7 +182,6 @@
 		});
 
 		p.then(function (cChapterIDObj) {
-				console.log(cChapterIDObj);
 				return cChapterIDObj;
 			})
 			.then(function (cChapterIDObj) {
@@ -183,7 +230,7 @@
 			arrRets.push({
 				chapterName: curChapterOption.innerText,
 				chapterValue: curChapterValue,
-				chapterProNum: i === 0 ? this.getChapterProNum(curChapterValue, i) : 0,
+				chapterProNum: i === 0 ? this.getChapterProNum(curChapterValue, i) : 0, // 默认是从第一章开始，就默认获取第一章的总题数
 				finishSign: false
 			});
 		}
@@ -359,8 +406,18 @@
 		promptInfoSelector: 'divInfo',
 		arrErrorChapters: [3]
 	};
+  
+  // 自主传参
+	// var answer = Answer(options);
 
-	var answer = Answer(options);
+	// 自主传参
+	var answer = Answer({
+		arrErrorChapters: [3]
+	});
+
+	// 不传参。采用默认参数
+	// var answer = Answer(); 
+
 	answer.start(); // 默认自动开启脚本执行
 
 	window.answer = answer;
